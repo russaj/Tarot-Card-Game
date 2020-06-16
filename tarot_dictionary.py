8c260a0eea94e6491c1f79b9d0928b9d2b90f78e
Andrew Russell <59831800+russaj@users.noreply.github.com>
2020-06-16 08:53:08 -0400

    Create tarot_dictionary.py

---
+++ tarot_dictionary.py
@@ -0,0 +1,9 @@
+Fool =	{
+  "face": "The Fool",
+  "Number": 0
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "New beginnings, beginner's mind, openness, freedom",
+  "Reverse Message: "N/A",
+  "Reverse Keywords": "Foolishness, recklessness, irresponsibility, naivety, lack of judgement"
+}
