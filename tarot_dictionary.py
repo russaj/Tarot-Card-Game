224136085d687baed0d2e023c33e3c20d26a3878
Andrew Russell <59831800+russaj@users.noreply.github.com>
2020-06-16 08:54:32 -0400

    Update tarot_dictionary.py

--- tarot_dictionary.py
+++ tarot_dictionary.py
@@ -4,6 +4,6 @@ Fool =	{
   "Suit": "Arcana Major",
   "Message": "N/A",
   "Keywords": "New beginnings, beginner's mind, openness, freedom",
-  "Reverse Message: "N/A",
+  "Reverse Message": "N/A",
   "Reverse Keywords": "Foolishness, recklessness, irresponsibility, naivety, lack of judgement"
 }
