15c992d6f9087f82685bd8127870648a181508bd
Andrew Russell <59831800+russaj@users.noreply.github.com>
2020-06-16 09:43:34 -0400

    Update tarot_dictionary.py

--- tarot_dictionary.py
+++ tarot_dictionary.py
@@ -7,3 +7,214 @@ fool =	{
   "Reverse Message": "N/A",
   "Reverse Keywords": "Foolishness, recklessness, irresponsibility, naivety, lack of judgement"
 }
+
+magician =	{
+  "face": "The Magician",
+  "Number": 1,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Consciousness, personal power, alchemy",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Blocked creative energy, unrealistic ambitions, inability to stay foced, manipulation"
+}
+
+highpriestess =	{
+  "face": "The High Priestess",
+  "Number": 2,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Unconscious, intuition, mystery, raw feeling (without verbal explanation)",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Mistrust of intuition, superficial knowledge, impatience, imbalance"
+}
+
+empress =	{
+  "face": "The Empress",
+  "Number": 3,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Creation, divine femininity, Being",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Unproductive, lack of growth, stagnation, neglect, procrastination, poor self-image"
+}
+
+emperor =	{
+  "face": "The Emperor",
+  "Number": 4,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Management, divine masculinity, Doing",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Chaos, impulsiveness, lack of self-discipline, misuse of power"
+}
+
+
+hierophant =	{
+  "face": "The Hierophant",
+  "Number": 5,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Social structure, social values, tradition",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Poor advice, narrow-mindedness, wrong course of action, restriction"
+}
+
+lovers =	{
+  "face": "The Lovers",
+  "Number": 6,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Individuation (realization of self through connection with another), love, a choice between love or fear",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Reluctance to commit, stagnation, discord, confusion, miscommunication, pressure"
+}
+
+chariot =	{
+  "face": "The Chariot",
+  "Number": 7,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Will power, control, material success",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Lack of direction, losing control, a halt in projects, defeat"
+}
+
+strength =	{
+  "face": "Strength",
+  "Number": 8,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Compassion, gentleness, courage to face fear and the wild mind",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Fear, self-doubt, insecurity, imbalance, anger"
+}
+
+hermit =	{
+  "face": "The Hermit",
+  "Number": 9,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Inner guidance, teacher/student relationship, solitude",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Isolation, loneliness, sadness, dishonesty, self-pity"
+}
+
+wheel =	{
+  "face": "The Wheel of Fortune",
+  "Number": 10,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Cycles, stepping back and seeing the big picture, change",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Setbacks, loss of control, wrong decisions, a period of difficulty"
+}
+
+justice =	{
+  "face": "Justice",
+  "Number": 11,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Karma, choices, cause and effect",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Injustice, irresponsibility, false accusations, lack of resolution, imbalance"
+}
+
+hangedman =	{
+  "face": "The Hanged Man",
+  "Number": 12,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Perspective, Sacrifice, acceptance, yielding & surrend",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Not letting go, narrow-mindedness, holding onto the material"
+}
+
+death =	{
+  "face": "Death",
+  "Number": 13,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Transformation, impermanence, a new beginning",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Stagnation, lethargy, indecision. destruction"
+}
+
+temperance =	{
+  "face": "Temperance",
+  "Number": 14,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Flexibility, present-moment responsiveness, integration",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Imbalance, fear of failure, recklessness, excess"
+}
+
+devil =	{
+  "face": "The Devil",
+  "Number": 15,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Self-empowerment, ambition, potential, determination, desire, temptation, indulgence",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Illusion, fear, addiction, anger obsession, self-destruction"
+}
+
+tower =	{
+  "face": "The Tower",
+  "Number": 16,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Revelation, crumbling of old structures, groundlessness, chaos",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Entrapment, oppression, strife, ongoing depression"
+}
+
+star =	{
+  "face": "The Star",
+  "Number": 17,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Hope, signs, the projection of one's inner knowing into the world",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Pessimism, sadness, dashed hopes, loneliness"
+}
+
+moon =	{
+  "face": "The Moon",
+  "Number": 18,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Unconscious, mystery, rejected aspects of the self, instinct",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Fear, insecurity, confusion, illusion, rejection of reality"
+}
+
+sun =	{
+  "face": "The Sun",
+  "Number": 19,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Consciousness, knowledge, personal enlightenment, truth, the True Self",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Wasted energy, fatigue, unclear future, delays"
+}
+
+judgement =	{
+  "face": "Judgement",
+  "Number": 20,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Life purpose, calling, legacy",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Indecision, self-doubt, lacking faith, procrastination"
+}
+
+world =	{
+  "face": "The World",
+  "Number": 21,
+  "Suit": "Arcana Major",
+  "Message": "N/A",
+  "Keywords": "Wholeness, total integration, oneness",
+  "Reverse Message": "N/A",
+  "Reverse Keywords": "Limitation, lack of vision, stagnation, disappointment"
+}
