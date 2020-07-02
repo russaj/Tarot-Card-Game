57aa2059dc91f5c7d1263068020083e6cf594090
Stephanie Simpson <56980886+stepsimp@users.noreply.github.com>
2020-07-02 17:42:49 -0400

    Create tarot2.py

---
+++ tarot2.py
@@ -0,0 +1,16 @@
+import tarot_dictionary as tarot_library
+
+# name = input("Hello. What is your name?")
+# print("Hello " + name + ". Welcome to the tarot game!")
+
+# ready = input(" Are you ready to have your fortune told? (Yes or No)")
+# if ready == "Yes":
+#     print("Great! Let's get started.")
+# else:
+#     print("It'll be fine - take a deep breath and let's get started.")
+
+number = input("Please pick a number between 1 and 22.")
+
+for x in tarot_library:
+    print(tarot_library["Keywords"])
+
