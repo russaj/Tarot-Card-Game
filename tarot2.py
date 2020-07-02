ee141da7237965b245b5b85a826747517956659e
Stephanie Simpson <56980886+stepsimp@users.noreply.github.com>
2020-07-02 17:48:58 -0400

    Update tarot2.py

--- tarot2.py
+++ tarot2.py
@@ -1,16 +1,16 @@
-import tarot_dictionary as tarot_deck
+import tarot_deck as tarot_library
 
-# name = input("Hello. What is your name?")
-# print("Hello " + name + ". Welcome to the tarot game!")
+name = input("Hello. What is your name?")
+print("Hello " + name + ". Welcome to the tarot game!")
 
-# ready = input(" Are you ready to have your fortune told? (Yes or No)")
-# if ready == "Yes":
-#     print("Great! Let's get started.")
-# else:
-#     print("It'll be fine - take a deep breath and let's get started.")
+ready = input("Are you ready to have your fortune told? (Yes or No)")
+if ready == "Yes":
+    print("Great! Let's get started.")
+else:
+    print("It'll be fine - take a deep breath and let's get started.")
 
 number = input("Please pick a number between 1 and 22.")
 
-for x in tarot_deck:
-    print(tarot_deck["Keywords"])
+for x in tarot_library:
+    print(tarot_library["Keywords"])
 
