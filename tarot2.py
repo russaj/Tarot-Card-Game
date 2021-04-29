5e89d8882f8e579ea1866953fe1d104e91225d9f
Andrew Russell <59831800+russaj@users.noreply.github.com>
2021-04-29 13:37:31 -0400

    Update tarot2.py
    made some efficiency changes and worked on Step #1

--- tarot2.py
+++ tarot2.py
@@ -2,6 +2,7 @@
 import tarot_dictionary
 import tarot_spread
 import random
+import zodiac
 # Module End
 
 # Full List of Tarot Cards
@@ -12,35 +13,50 @@ def full_deck():
         list_cards.append(x)
     return list_cards
 
+# Select the Spread you want
+def spread_select():
+    choiceSelected = False
+    while choiceSelected == False:
+        print("Are you interested in a One Card Oracle reading (A) or a Three Card Oracle Reading (B)")
+        card_spread = input().upper()
+        if card_spread == "A":
+            #oracle_one_reading
+            usedSpread = tarot_spread.oracleSpread
+            choiceSelected = True
+        elif card_spread == "B":
+            #oracle_three_reading
+            usedSpread = tarot_spread.threeSpread
+            choiceSelected = True
+        else:
+            print("Stop wasting my time! Pick one!")
+            print("")
+    return usedSpread
+
 card_face = []
 card_meaning = []
 
 #Step 1: Meet the guest
-# name = input("Hello. What is your name?")
-# print("Hello " + name + ". Welcome to the tarot game!")
+name = input("Hello. What is your name?")
 
-# ready = input(" Are you ready to have your fortune told? (Yes or No)")
-# if ready == "Yes":
-#     print("Great! Let's get started.")
-# else:
-#     print("It'll be fine - take a deep breath and let's get started.")
-
-
-#Step 2: Select type of reading from tarot_spread document
-print("Are you interested in a One Card Oracle reading (A) or a Three Card Oracle Reading (B)")
-card_spread = input().upper()
-if card_spread == "A":
-    #oracle_one_reading
-    usedSpread = tarot_spread.oracleSpread
-elif card_spread == "B":
-    #oracle_three_reading
-    usedSpread = tarot_spread.threeSpread
+print("Hello " + name + ". Welcome to the tarot game!")
+birthDate = input("What month/day were you born (numerically mmdd)?")
+if birthDate.isnumeric() == False:
+    print("Please provide a valid birthday.")
 else:
-    print("Stop wasting my time! Pick one!")
-#print(usedSpread)
-dictionaryUsed = usedSpread
+    if zodiac.zodiacSign(birthDate) == zodiac.currentZodiacSign(zodiac.currentDate):
+        print("Wow, it's your birth season!")
+    print("Looks like you're a", zodiac.zodiacSign(birthDate))
+    ready = input(" Are you ready to have your fortune told? (Yes or No)")
+    if ready == "Yes":
+        print("Great! Let's get started.")
+    else:
+        #revise language here, not so forceful?
+        print("It'll be fine - take a deep breath and let's get started.")
 
-#Step 3: Selected card from tarot_deck and determining its orientation
+#Step 2: Select type of reading > incorporate other file of readings?
+#Think about putting a While Loop Here to circle through if invalid answer given
+dictionaryUsed = spread_select()
+
 totalPullcards = dictionaryUsed['totalCards']
 card = random.sample(range(0,21),totalPullcards)
 for x in card:
@@ -57,7 +73,6 @@ for x in card:
     card_face.append(face)
     card_meaning.append(meaning)
 
-#Step 4: Spitting back out the values
 cardDescriptionValue = []
 for key, value in dictionaryUsed.items():
     if key == 'name':
@@ -68,9 +83,8 @@ for key, value in dictionaryUsed.items():
         cardDescriptionValue.append(value)
 
 x = 0
-y = x+1
 while x < totalPullcards:
-    y = x+1
-    print("The number", y ,"card is", card_face[x])
+    print("The number", (x+1) ,"card is", card_face[x])
     print(cardDescriptionValue[x],"is",card_meaning[x])
+    print("")
     x += 1
